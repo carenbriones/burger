7c1f5ad62d11d3b0501298786a23a53d95223f39
Caren B <carenpbriones@gmail.com>
2019-10-09 01:27:09 -0700

    Added classes to handlebars file elements to write CSS for basic layout

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
 
         var newBurger = {
             burger_name: $("#burger-name").val().trim(),
-            devoured: $("[name=devoured]:checked").val().trim()
+            devoured: 0
         };
 
         // Send the POST request.
